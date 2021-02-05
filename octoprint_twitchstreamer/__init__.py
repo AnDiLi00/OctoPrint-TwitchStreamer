38ff4c0ad00a2e27590639ddf0d38518a6de7fd9
AnDiLi00 <pecuch.andreas@gmail.com>
2021-02-05 11:35:34 +0100

    reformated files

--- octoprint_twitchstreamer/__init__.py
+++ octoprint_twitchstreamer/__init__.py
@@ -42,7 +42,7 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 	font = ""
 	font_size = 0
 
-	timer = None
+	update_timer = None
 	end_timer = None
 
 	process = None
@@ -60,8 +60,8 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 			status_x=13,
 			status_y=625,
 			graphic_show=True,
-			graphic_x=0,
-			graphic_y=0,
+			graphic_x=1202,
+			graphic_y=640,
 			graphic_file="overlay.png",
 			webcam_path="http://octopi.local/webcam/?action=stream",
 			twitch_key="",
@@ -74,44 +74,44 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 	def on_settings_save(self, data):
 		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
 
-		self.check(self._settings.get(["folder"]),
-				   self._settings.get(["temperature_show"]),
-				   self._settings.get(["temperature_x"]),
-				   self._settings.get(["temperature_y"]),
-				   self._settings.get(["status_show"]),
-				   self._settings.get(["status_x"]),
-				   self._settings.get(["status_y"]),
-				   self._settings.get(["graphic_show"]),
-				   self._settings.get(["graphic_x"]),
-				   self._settings.get(["graphic_y"]),
-				   self._settings.get(["graphic_file"]),
-				   self._settings.get(["webcam_path"]),
-				   self._settings.get(["twitch_key"]),
-				   self._settings.get(["quality"]),
-				   self._settings.get(["bitrate"]),
-				   self._settings.get(["font"]),
-				   self._settings.get(["font_size"]))
+		self.check_settings(self._settings.get(["folder"]),
+							self._settings.get(["temperature_show"]),
+							self._settings.get(["temperature_x"]),
+							self._settings.get(["temperature_y"]),
+							self._settings.get(["status_show"]),
+							self._settings.get(["status_x"]),
+							self._settings.get(["status_y"]),
+							self._settings.get(["graphic_show"]),
+							self._settings.get(["graphic_x"]),
+							self._settings.get(["graphic_y"]),
+							self._settings.get(["graphic_file"]),
+							self._settings.get(["webcam_path"]),
+							self._settings.get(["twitch_key"]),
+							self._settings.get(["quality"]),
+							self._settings.get(["bitrate"]),
+							self._settings.get(["font"]),
+							self._settings.get(["font_size"]))
 
 	##~~ StartupPlugin mixin
 
 	def on_after_startup(self):
-		self.check(self._settings.get(["folder"]),
-				   self._settings.get(["temperature_show"]),
-				   self._settings.get(["temperature_x"]),
-				   self._settings.get(["temperature_y"]),
-				   self._settings.get(["status_show"]),
-				   self._settings.get(["status_x"]),
-				   self._settings.get(["status_y"]),
-				   self._settings.get(["graphic_show"]),
-				   self._settings.get(["graphic_x"]),
-				   self._settings.get(["graphic_y"]),
-				   self._settings.get(["graphic_file"]),
-				   self._settings.get(["webcam_path"]),
-				   self._settings.get(["twitch_key"]),
-				   self._settings.get(["quality"]),
-				   self._settings.get(["bitrate"]),
-				   self._settings.get(["font"]),
-				   self._settings.get(["font_size"]))
+		self.check_settings(self._settings.get(["folder"]),
+							self._settings.get(["temperature_show"]),
+							self._settings.get(["temperature_x"]),
+							self._settings.get(["temperature_y"]),
+							self._settings.get(["status_show"]),
+							self._settings.get(["status_x"]),
+							self._settings.get(["status_y"]),
+							self._settings.get(["graphic_show"]),
+							self._settings.get(["graphic_x"]),
+							self._settings.get(["graphic_y"]),
+							self._settings.get(["graphic_file"]),
+							self._settings.get(["webcam_path"]),
+							self._settings.get(["twitch_key"]),
+							self._settings.get(["quality"]),
+							self._settings.get(["bitrate"]),
+							self._settings.get(["font"]),
+							self._settings.get(["font_size"]))
 
 	##~~ TemplatePlugin mixin
 
@@ -122,38 +122,20 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 
 	def on_event(self, event, payload):
 		if event == Events.PRINT_STARTED:
-			self._logger.info("received event PRINT STARTED")
-			self.print_start()
+			self._logger.info("on_event - Events.PRINT_STARTED")
+		# self.print_start()
 		elif event == Events.PRINT_FAILED:
-			self._logger.info("received event PRINT FAILED")
-			self.print_end(True)
+			self._logger.info("on_event - Events.PRINT_FAILED")
+		# self.print_end(True)
 		elif event == Events.PRINT_DONE:
-			self._logger.info("received event PRINT DONE")
-			self.print_end(False)
+			self._logger.info("on_event - Events.PRINT_DONE")
+
+	# self.print_end(False)
 
 	##~~ Class specific
 
-	def check(self,
-			  new_folder,
-			  new_temp,
-			  new_temp_x,
-			  new_temp_y,
-			  new_status,
-			  new_status_x,
-			  new_status_y,
-			  new_graphic,
-			  new_graphic_x,
-			  new_graphic_y,
-			  new_graphic_file,
-			  new_webcam_path,
-			  new_twitch_key,
-			  new_quality,
-			  new_bitrate,
-			  new_font,
-			  new_font_size):
-		changed = False
-
-		self._logger.info("current settings:")
+	def check_settings(self, new_folder, new_temp, new_temp_x, new_temp_y, new_status, new_status_x, new_status_y, new_graphic, new_graphic_x, new_graphic_y, new_graphic_file, new_webcam_path, new_twitch_key, new_quality, new_bitrate, new_font, new_font_size):
+		self._logger.info("check_settings - new settings")
 		self._logger.info("-- folder={}".format(new_folder))
 		self._logger.info("-- temperature")
 		self._logger.info("-- -- show={}".format(new_temp))
@@ -174,6 +156,8 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 		self._logger.info("-- bitrate={}".format(new_bitrate))
 		self._logger.info("-- font={}".format(new_font))
 		self._logger.info("-- font size={}".format(new_font_size))
+
+		changed = False
 
 		if self.folder != new_folder:
 			try:
@@ -247,122 +231,115 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 			self.font_size = new_font_size
 			changed = True
 
-		if changed:
-			if self.streaming:
-				self.print_start()
+		self.updatetimer_start()
 
-	def start_timer(self):
-		self.stop_timer()
+		if changed and self.streaming:
+			self.print_start()
 
-		self.timer = RepeatedTimer(5.0, self.update_values, run_first=True)
-		self.timer.start()
-
-	def stop_timer(self):
-		if self.timer:
-			self.timer.cancel()
-			self.timer = None
-
-	def update_values(self):
-		self.status_data = self._printer.get_current_data()
-		self.temperature_data = self._printer.get_current_temperatures()
-
-		self.write_temperature()
-		self.write_status()
-
-	def write_temperature(self):
-		write_data = ""
+	def update_temperature(self):
+		data = ""
 
 		if self.temperature_data:
 			if "tool0" in self.temperature_data:
-				write_data += "nozzle: "
-				write_data += str("{:.1f}".format(self.temperature_data["tool0"]["actual"])).rjust(5)
-				write_data += "°C of "
-				write_data += str("{:.1f}".format(self.temperature_data["tool0"]["target"])).rjust(5)
-				write_data += "°C"
+				data += "nozzle: "
+				data += str("{:.1f}".format(self.temperature_data["tool0"]["actual"])).rjust(5)
+				data += "°C of "
+				data += str("{:.1f}".format(self.temperature_data["tool0"]["target"])).rjust(5)
+				data += "°C"
 
-			if write_data:
-				write_data += "\n"
+			if data:
+				data += "\n"
 
 			if "bed" in self.temperature_data:
-				write_data += "bed:    "
-				write_data += str("{:.1f}".format(self.temperature_data["bed"]["actual"])).rjust(5)
-				write_data += "°C of "
-				write_data += str("{:.1f}".format(self.temperature_data["bed"]["target"])).rjust(5)
-				write_data += "°C"
+				data += "bed:    "
+				data += str("{:.1f}".format(self.temperature_data["bed"]["actual"])).rjust(5)
+				data += "°C of "
+				data += str("{:.1f}".format(self.temperature_data["bed"]["target"])).rjust(5)
+				data += "°C"
 
-		self.touch(self.folder, self.temperature_file, write_data)
+		self.touch_file(self.folder, self.temperature_file, data)
 
-	def write_status(self):
-		write_data = ""
+	def update_status(self):
+		data = ""
 
 		if self.status_data:
-			flag_paused = False
-			flag_printing = False
-			flag_pausing = False
-			flag_canceling = False
-			flag_finishing = False
+			printing_state = False
 
 			if "state" in self.status_data:
 				state_dict = self.status_data["state"]
 				flags_dict = state_dict["flags"]
 
-				flag_paused = flags_dict["paused"]
-				flag_printing = flags_dict["printing"]
-				flag_pausing = flags_dict["pausing"]
-				flag_canceling = flags_dict["cancelling"]
-				flag_finishing = flags_dict["finishing"]
+				if flags_dict["cancelling"] or flags_dict["finishing"] or flags_dict["paused"] or flags_dict["pausing"] or flags_dict["printing"]:
+					printing_state = True
 
-				write_data += "state:   "
-				write_data += state_dict["text"].lower()
+				data += "state:   "
+				data += state_dict["text"].lower()
 
-			if flag_paused or flag_printing or flag_pausing or flag_canceling or flag_finishing:
-				write_data += "\n"
+			if printing_state:
+				data += "\n"
 
 				if "job" in self.status_data:
 					job_dict = self.status_data["job"]
 
 					if "file" in job_dict:
-						if job_dict["file"]["name"] is None:
-							write_data += "file:    -"
+						if job_dict["file"]["name"]:
+							data += "file:    "
+							data += job_dict["file"]["name"]
 						else:
-							write_data += "file:    "
-							write_data += job_dict["file"]["name"]
+							data += "file:    -"
 					else:
-						write_data += "file:    -"
+						data += "file:    -"
 
-					write_data += "\n"
+					data += "\n"
 
 				if "progress" in self.status_data:
 					progress_dict = self.status_data["progress"]
 					print_time = progress_dict["printTime"]
 					print_time_left = progress_dict["printTimeLeft"]
 
-					if print_time is None:
-						write_data += "elapsed: 0s"
+					if print_time:
+						data += "elapsed: "
+						data += self.sec_to_text(print_time)
 					else:
-						write_data += "elapsed: "
-						write_data += self.sec_to_text(print_time)
+						data += "elapsed: 0s"
 
-					write_data += "\n"
+					data += "\n"
 
-					if print_time_left is None:
-						write_data += "left:    0s"
+					if print_time_left:
+						data += "left:    "
+						data += self.sec_to_text(print_time_left)
 					else:
-						write_data += "left:    "
-						write_data += self.sec_to_text(print_time_left)
+						data += "left:    0s"
 
-					write_data += "\n"
+					data += "\n"
 
-					if print_time is None or print_time_left is None:
-						write_data += "percent: 0.0\%"
-					else:
+					if print_time and print_time_left:
 						float_percent = 100.0 * float(print_time / (print_time_left + print_time))
-
-						write_data += "percent: {:.1f}\%".format(float_percent)
+						data += "percent: {:.1f}\%".format(float_percent)
+					else:
+						data += "percent: 0.0\%"
 			else:
-				write_data = "\n\n\n\n" + write_data
+				data = "\n\n\n\n" + data
 
-		self.touch(self.folder, self.status_file, write_data)
+		self.touch_file(self.folder, self.status_file, data)
+
+	def update_values(self):
+		self.status_data = self._printer.get_current_data()
+		self.temperature_data = self._printer.get_current_temperatures()
+
+		self.update_temperature()
+		self.update_status()
+
+	def updatetimer_stop(self):
+		if self.update_timer:
+			self.update_timer.cancel()
+			self.update_timer = None
+
+	def updatetimer_start(self):
+		self.updatetimer_stop()
+
+		self.update_timer = RepeatedTimer(5.0, self.update_values, run_first=True)
+		self.update_timer.start()
 
 	def stream_start(self):
 		filepath_temperature = self.folder + self.temperature_file
@@ -375,12 +352,16 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 			command += "-filter_complex \"[0:v]"
 
 		if self.temperature_show:
-			command += "drawtext=fontfile={self.font}:textfile={filepath_temperature}:x={self.temperature_x}:y={self.temperature_y}:reload=1:fontcolor=white:fontsize={self.font_size}[vtxt1]"
+			command += "drawtext=fontfile={self.font}:textfile={filepath_temperature}:"
+			command += "x={self.temperature_x}:y={self.temperature_y}:reload=1:"
+			command += "fontcolor=white:fontsize={self.font_size}[vtxt1]"
 			if self.status_show or self.graphic_show:
 				command += ";[vtxt1]"
 
 		if self.status_show:
-			command += "drawtext=fontfile={self.font}:textfile={filepath_status}:x={self.status_x}:y={self.status_y}:reload=1:fontcolor=white:fontsize={self.font_size}[vtxt2]"
+			command += "drawtext=fontfile={self.font}:textfile={filepath_status}:"
+			command += "x={self.status_x}:y={self.status_y}:reload=1:"
+			command += "fontcolor=white:fontsize={self.font_size}[vtxt2]"
 			if self.graphic_show:
 				command += ";[vtxt2]"
 
@@ -389,8 +370,8 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 		elif self.temperature_show or self.status_show:
 			command += "\" "
 
-		command += "-vcodec libx264 -pix_fmt yuv420p -preset {self.quality} -g 20 -b:v {self.bitrate}k -threads 0 -bufsize 512k "
-		command += "-map \""
+		command += "-vcodec libx264 -pix_fmt yuv420p -threads 0 -bufsize 512k "
+		command += "-preset {self.quality} -g 20 -b:v {self.bitrate}k -map \""
 
 		if self.graphic_show:
 			command += "[out]"
@@ -402,39 +383,34 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 			command += "[0:v]"
 
 		command += "\" -f flv rtmp://live.twitch.tv/app/{self.twitch_key}"
-		formated_command = command.format(**locals())
 
-		self.process = subprocess.Popen(formated_command, shell=True)
+		command_formated = command.format(**locals())
+		#self.process = subprocess.Popen(command_formated, stdout=subprocess.PIPE, shell=True)
 
-		self._logger.info("stream start - command={}".format(formated_command))
+		self._logger.info("stream_start - command={}".format(command_formated))
 
 	def stream_end(self):
 		if self.process:
 			self.process.terminate()
 			self.process = None
 
-			self._logger.info("stream end")
+		self._logger.info("stream_end")
 
 	def print_start(self):
-		self._logger.info("print start")
-
-		self.streaming = True
-
-		self.start_timer()
+		self._logger.info("print_start")
 
 		self.stream_end()
 
 		if self.end_timer:
 			self.end_timer.cancel()
 
+		self.streaming = True
 		self.stream_start()
 
 	def print_end(self, forced):
-		self._logger.info("print end - forced={}".format(forced))
+		self._logger.info("print_end - forced={}".format(forced))
 
 		self.streaming = False
-
-		self.stop_timer()
 
 		if forced:
 			self.stream_end()
@@ -442,15 +418,68 @@ class TwitchstreamerPlugin(octoprint.plugin.SettingsPlugin,
 			if self.end_timer:
 				self.end_timer.reset()
 			else:
-				self.end_timer = ResettableTimer(120.0, self.stream_end)
+				self.end_timer = ResettableTimer(60.0, self.stream_end)
 				self.end_timer.start()
 
-	@staticmethod
-	def touch(path, filename, data):
-		if filename:
-			file_tmp = open(os.path.join(path, filename), 'w+')
-			file_tmp.write(data)
-			file_tmp.close()
+	def touch_path(self, path):
+		if path:
+			if os.path.exists(path):
+				pass
+			else:
+				try:
+					os.makedirs(path, exist_ok=True)
+				except OSError as error:
+					self._logger.error("path '{}' couldn't be created - errno:{}".format(path, error.errno))
+		else:
+			self._logger.error("path not valid")
+
+	def remove_path(self, path):
+		if path:
+			if os.path.exists(path) and os.path.isdir(path):
+				if not os.listdir(path):
+					try:
+						os.rmdir(path)
+					except OSError as error:
+						self._logger.error("path '{}' couldn't be removed - errno:{}".format(path, error.errno))
+			else:
+				self._logger.error("path does not exist")
+		else:
+			self._logger.error("path not valid")
+
+	def touch_file(self, path, file, data):
+		joined = os.path.join(path, file)
+
+		if path:
+			if file:
+				self.touch_path(path)
+
+				try:
+					file_tmp = open(joined, 'w+')
+					file_tmp.write(data)
+					file_tmp.close()
+				except OSError as error:
+					self._logger.error("file '{}' couldn't be created - errno:{}".format(joined, error.errno))
+			else:
+				self._logger.error("file not valid")
+		else:
+			self._logger.error("path not valid")
+
+	def remove_file(self, path, file):
+		joined = os.path.join(path, file)
+
+		if path:
+			if file:
+				if os.path.exists(joined):
+					try:
+						os.remove(joined)
+					except OSError as error:
+						self._logger.error("file '{}' couldn't be removed - errno:{}".format(joined, error.errno))
+				else:
+					self._logger.error("file does not exist")
+			else:
+				self._logger.error("file not valid")
+		else:
+			self._logger.error("path not valid")
 
 	@staticmethod
 	def sec_to_text(seconds):
